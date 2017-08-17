a02e91b24e45bf093c0c4b80e6aa4786ba6940ab
Tiago Barbosa <tfreirebar@gmail.com>
2017-08-17 00:25:28 -0300

    feat: add repo

---
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/utils/Constants.java
@@ -0,0 +1,32 @@
+package com.udacity.firebase.shoppinglistplusplus.utils;
+
+import com.udacity.firebase.shoppinglistplusplus.BuildConfig;
+
+/**
+ * Constants class store most important strings and paths of the app
+ */
+public final class Constants {
+
+    /**
+     * Constants related to locations in Firebase, such as the name of the node
+     * where active lists are stored (ie "activeLists")
+     */
+
+
+    /**
+     * Constants for Firebase object properties
+     */
+
+
+    /**
+     * Constants for Firebase URL
+     */
+    public static final String FIREBASE_URL = BuildConfig.UNIQUE_FIREBASE_ROOT_URL;
+
+
+    /**
+     * Constants for bundles, extras and shared preferences keys
+     */
+
+
+}
